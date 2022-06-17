fea284da4bb6c6be64291b350cf054476d629a95
LatellaL <83371530+LatellaL@users.noreply.github.com>
2022-06-17 10:43:32 -0400

    Create index.js
    Delete method

---
+++ src/index.js
@@ -0,0 +1,146 @@
+const endPoint = "http://localhost:3000/api/v1/games";
+const gameForm = document.getElementById("game-form");
+const scoreModal = document.getElementById("scoreModal");
+const buttonModal = document.getElementById("buttonModal");
+const pause = document.getElementById("pause-game");
+const deleteButton = document.querySelector(".delete-button");
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    loadGames()
+    submitForm()
+    pauseGame()
+    eventDelegation()
+})
+
+function gameOver() {
+    scoreModal.style.display = "flex";
+    document.getElementById("game-over").style.display = "flex";
+    clickToClose()
+}
+
+function gameWin() {
+    scoreModal.style.display = "flex";
+    document.getElementById("game-win").style.display = "flex";
+    clickToClose()
+}
+
+function modalClear() {
+    scoreModal.style.display = "none";
+    replay()
+}
+
+function clickToClose() {
+    const closeX = document.getElementById("close-modal")
+    closeX.addEventListener("click", handleCloseClick)
+    window.addEventListener("click", function(event) {
+        if (event.target === scoreModal) {
+            handleCloseClick()
+          }
+    })
+}
+
+
+function handleCloseClick() {
+    scoreModal.style.display = "none"
+    replay()
+}
+
+function replay() {
+    buttonModal.style.display = "flex";
+    replayButton = document.getElementById("replay-button")
+    replayButton.addEventListener("click", canvasReload)
+}
+
+
+function loadGames() {
+    fetch(endPoint)
+    .then(resp => resp.json())
+    .then(games => {
+        addGamesToPage(games)
+    })
+}
+
+function addGamesToPage(games) {
+    document.querySelector("#score-board-container").innerHTML =            
+        `<tr>
+            <th>NAME:</th>
+            <th>SCORE:</th>
+        </tr>`
+        
+    games.data.forEach(game => {
+        attachGame(game)
+    });
+}
+
+function attachGame(game) {
+    const newGame = new Game(game.id, game.attributes);
+    document.querySelector("#score-board-container").innerHTML += newGame.renderGame();
+}
+
+
+function submitForm(){
+    gameForm.addEventListener("submit", (e) => formHandler(e))
+}
+
+function formHandler(e) {
+    e.preventDefault()
+    const formScorePhrase = document.getElementById("score").innerText
+    const formScore = parseInt(formScorePhrase.replace("Score: ", ""))
+    const formName = document.getElementById("name").value
+    postFetch(formScore, formName)
+}
+
+function postFetch(score, user_name){
+    const bodyData = 
+    // this object created to satisfy strong params on back end (:game is required)
+        {game:
+            {score, user_name}
+        }
+
+    fetch(endPoint, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(bodyData),
+    })
+    .then(resp => resp.json())
+    .then(game => {
+        if (!game.errors) {
+            loadGames()
+        } else {
+            throw new Error (`${game.errors}`)
+        }
+    })
+    .catch(alert)
+    modalClear()
+}
+
+function eventDelegation() {
+    const scoreList = document.querySelector(".score-table")
+    scoreList.addEventListener("click", function(e){
+        if (e.target.className == "delete-button button") {
+            console.log("you clicked delete")
+            const gameID = e.target.id
+            deleteGame(gameID)
+        }
+    })
+}
+
+async function deleteGame(id){
+    const resp = await fetch(`${endPoint}/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    const data = await resp.json()
+    loadGames()
+  }
+
+function pauseGame() {
+    pause.addEventListener("click", handlePause)
+}
+
+function handlePause() {
+    dx === 0 ? ballGo() : ballStop()
+}
