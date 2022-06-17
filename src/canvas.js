fde6e26e8ad7e6a33e1fbe5a30c798068eb77c5c
LatellaL <83371530+LatellaL@users.noreply.github.com>
2022-06-17 10:54:08 -0400

    pause game button

--- src/canvas.js
+++ src/canvas.js
@@ -109,7 +109,7 @@ function drawBall() {
     ctx.fill();
     ctx.closePath();
 }
-
+ 
 function drawPaddle() {
     ctx.beginPath();
     ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
